b8a4c3d9d6050a9035c048b70afd1bab9c9627fa
ICEY <icey@icey.tech>
2020-03-23 11:45:23 +0800

    [comparator] add comparator

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,56 @@
+#![feature(fn_traits)]
+
+use std::cmp::Ordering;
+use std::marker::PhantomData;
+
 mod encode;
 mod error;
 mod table;
 mod partition;
+
+pub trait Comparator {
+    fn compare(lhs: &[u8], rhs: &[u8]) -> Ordering;
+}
+
+pub struct DefaultComparator();
+
+impl Comparator for DefaultComparator {
+    fn compare(lhs: &[u8], rhs: &[u8]) -> Ordering {
+        lhs.cmp(rhs)
+    }
+}
+
+pub(crate) struct KVPair<Comp: Comparator> {
+    key: Vec<u8>,
+    value: Vec<u8>,
+    phantom: PhantomData<Comp>
+}
+
+impl<Comp: Comparator> KVPair<Comp> {
+    fn new(key: Vec<u8>, value: Vec<u8>) -> Self {
+        Self { key, value, phantom: PhantomData }
+    }
+}
+
+impl<Comp: Comparator> Ord for KVPair<Comp> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Comp::compare(&self.key, &other.key)
+    }
+}
+
+impl<Comp: Comparator> PartialOrd for KVPair<Comp> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<Comp: Comparator> PartialEq for KVPair<Comp> {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl<Comp: Comparator> Eq for KVPair<Comp> {}
 
 #[cfg(test)]
 mod tests {
--- src/partition/mod.rs
+++ src/partition/mod.rs
@@ -1,11 +1,11 @@
 use std::collections::BTreeMap;
 use crate::table::Table;
 use crate::table::cache::TableCacheManager;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 
 struct Partition {
-    mem_table: BTreeMap<Vec<u8>, Vec<u8>>,
-    imm_table: BTreeMap<Vec<u8>, Vec<u8>>,
+    mem_table: RwLock<BTreeMap<Vec<u8>, Vec<u8>>>,
+    imm_table: Mutex<Option<BTreeMap<Vec<u8>, Vec<u8>>>>,
     levels: Vec<Vec<Box<dyn Table>>>,
-    cache_manager: Arc<Mutex<TableCacheManager>>
+    cache_manager: Mutex<TableCacheManager>
 }
