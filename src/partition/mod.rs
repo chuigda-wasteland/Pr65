257b68001ede55ba6aec2a82a9090d4a831a107a
ICEY <icey@icey.tech>
2020-04-09 21:16:08 +0800

    [partition] I forgot notify

--- src/partition/mod.rs
+++ src/partition/mod.rs
@@ -258,6 +258,7 @@ impl<'a, Comp: 'static + Comparator> ArcPartition<'a, Comp> {
             data.levels[0].add_file(table);
             // TODO flush metadata onto disk
             let _ = data.imm_table.take();
+            partition.condvar.notify_one();
         }
         // TODO check if it is required to schedule another compaction from level 1 to level 2
     }
